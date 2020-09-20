b97db6c7cc70b0ad018fd78592d8c2db6f2aa5ca
luisfalconmx <me@luisfalconmx.com>
2020-09-19 22:16:55 -0500

    Creation of the functionality to show the x icon when there is text in the input

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log('hello world')
+const input = document.getElementById('main__input')
+const btn_close = document.getElementById('main__icon--cross')
+
+input.addEventListener('input', () => {
+
+    if (input.value == '') {
+        btn_close.classList.remove('main__icon--show')
+    }
+
+    else {
+        btn_close.classList.add('main__icon--show')
+    }
+
+})
