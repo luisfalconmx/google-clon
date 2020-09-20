3ca3e1e5ff3cffff2e88eba6b2643c5d22705a80
luisfalconmx <me@luisfalconmx.com>
2020-09-20 02:09:20 -0500

    Creation of the search functionality for the buttons 'Search in google' and 'I feel lucky'

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const input = document.getElementById('main__input')
 const btn_close = document.getElementById('main__icon--cross')
+const btn_search = document.getElementById('main__button--search')
+const btn_lucky = document.getElementById('main__button--lucky')
 
+// Show / Hide x button
 input.addEventListener('input', () => {
     if (input.value == '') {
         btn_close.classList.remove('main__icon--show')
@@ -10,11 +13,13 @@ input.addEventListener('input', () => {
     }
 })
 
+// Delete input values ​​by clicking on the x button
 btn_close.addEventListener('click', () => {
     input.value = '';
     btn_close.classList.remove('main__icon--show')
 })
 
+// Do the search when the enter key is pressed and the input has values
 window.addEventListener('keydown', (e) => {
     if (e.key === 'Enter') {
         e.preventDefault();
@@ -23,5 +28,25 @@ window.addEventListener('keydown', (e) => {
             const search_value = input.value.replace(/\s/g, '+')
             window.location.href = `https://www.google.com/search?q=${search_value}`;
         }
+    }
+})
+
+// Do the search when the search button is pressed and the input has values
+btn_search.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    if (input.value !== ''){
+        const search_value = input.value.replace(/\s/g, '+')
+        window.location.href = `https://www.google.com/search?q=${search_value}`;
+    }
+})
+
+// Do the search when the "I'm going to be lucky" button is pressed and the input has values
+btn_lucky.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    if (input.value !== ''){
+        const search_value = input.value.replace(/\s/g, '+')
+        window.location.href = `https://www.google.com/search?q=${search_value}&btnI`;
     }
 })
