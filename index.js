a9e23619f839eab89e9212af1f50a15a076f7946
luisfalconmx <me@luisfalconmx.com>
2020-09-20 01:16:43 -0500

    Creation of the search system

--- index.js
+++ index.js
@@ -2,18 +2,26 @@ const input = document.getElementById('main__input')
 const btn_close = document.getElementById('main__icon--cross')
 
 input.addEventListener('input', () => {
-
     if (input.value == '') {
         btn_close.classList.remove('main__icon--show')
     }
-
     else {
         btn_close.classList.add('main__icon--show')
     }
-
 })
 
 btn_close.addEventListener('click', () => {
     input.value = '';
     btn_close.classList.remove('main__icon--show')
+})
+
+window.addEventListener('keydown', (e) => {
+    if (e.key === 'Enter') {
+        e.preventDefault();
+
+        if (input.value !== ''){
+            const search_value = input.value.replace(/\s/g, '+')
+            window.location.href = `https://www.google.com/search?q=${search_value}`;
+        }
+    }
 })
