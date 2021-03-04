c47cb7af4208484fe8ca176802b15152cc8625b8
luisfalconmx <me@luisfalconmx.com>
2021-03-03 21:57:03 -0600

    feat: gulp updated with new configuration

--- gulpfile.js
+++ gulpfile.js
@@ -1,49 +1,65 @@
-const gulp = require("gulp");
-const htmlmin = require("gulp-htmlmin");
-const cleanCSS = require("gulp-clean-css");
-const terser = require("gulp-terser");
-const clean = require("gulp-clean");
-const ghPages = require("gulp-gh-pages");
+const gulp = require('gulp');
+const cleanCSS = require('gulp-clean-css');
+const htmlmin = require('gulp-htmlmin');
+const terser = require('gulp-terser')
+const babel = require('gulp-babel');
+const browserSync = require('browser-sync').create();
+const reload = browserSync.reload;
+const clean = require('gulp-clean');
+const imagemin = require('gulp-imagemin')
 
-gulp.task("minify-html", () => {
+gulp.task('compile:html', () => {
   return gulp
-    .src("src/html/*.html")
+    .src('./src/html/**/*.html')
     .pipe(htmlmin({ collapseWhitespace: true }))
-    .pipe(gulp.dest("dist"));
-});
+    .pipe(gulp.dest('./dist/'))
+})
 
-gulp.task("minify-css", () => {
+gulp.task('compile:css', () => {
   return gulp
-    .src("src/css/*.css")
-    .pipe(cleanCSS({ compatibility: "ie8" }))
-    .pipe(gulp.dest("dist/"));
+    .src('./src/css/**/*.css')
+    .pipe(cleanCSS({compatibility: 'ie8'}))
+    .pipe(gulp.dest('./dist/'))
+})
+
+gulp.task('compile:js', () => {
+  return gulp
+    .src('./src/js/**/*.js')
+    .pipe(babel({ presets: ['@babel/preset-env'] }))
+    .pipe(terser())
+    .pipe(gulp.dest('./dist/'))
+})
+
+gulp.task('compile:images', () => {
+  return gulp
+    .src('./src/images/**/*')
+    .pipe(imagemin())
+    .pipe(gulp.dest('./dist/images/'))
+})
+
+gulp.task('create:dist', () => {
+  return gulp
+    .src('./')
+    .pipe(gulp.dest('./dist/'))
+})
+
+gulp.task('clean:dist', () => {
+  return gulp
+    .src('./dist/')
+    .pipe(clean())
+})
+
+gulp.task('server', () => {
+  browserSync.init({
+    server: {
+      baseDir: './dist/'
+    }
+  });
+
+  gulp.watch('./src/html/**/*.html', gulp.series(['compile:html'])).on('change', reload);
+  gulp.watch('./src/css/**/*.css', gulp.series(['compile:css'])).on('change', reload);
+  gulp.watch('./src/js/**/*.js', gulp.series(['compile:js'])).on('change', reload);
 });
 
-gulp.task("minify-js", () => {
-  return gulp.src("src/js/*.js").pipe(terser()).pipe(gulp.dest("dist/"));
-});
-
-gulp.task("copy-images", () => {
-  return gulp.src("./src/images/**/*").pipe(gulp.dest("./dist/images"));
-});
-
-gulp.task("clean", () => {
-  return gulp.src("dist", { read: false }).pipe(clean());
-});
-
-gulp.task("github-pages", function () {
-  return gulp.src("./dist/**/*").pipe(ghPages());
-});
-
-gulp.task(
-  "build",
-  gulp.series([
-    "clean",
-    "minify-html",
-    "minify-css",
-    "minify-js",
-    "copy-images",
-  ])
-);
-
-gulp.task("deploy", gulp.series(["build", "github-pages"]));
+gulp.task('dev', gulp.series(['create:dist', 'clean:dist', 'compile:html', 'compile:css', 'compile:js', 'compile:images', 'server']));
+gulp.task('build', gulp.series(['create:dist', 'clean:dist', 'compile:html', 'compile:css', 'compile:images', 'compile:js']));
